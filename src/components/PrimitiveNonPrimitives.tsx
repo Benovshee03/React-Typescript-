a193fd0e4535283612a3711186f6bbedcf81d0ad
Benovshe Meherremova <benovshe@Benovshes-MacBook-Pro.local>
2024-02-13 17:35:28 +0400

    second commit

--- src/components/PrimitiveNonPrimitives.tsx
+++ src/components/PrimitiveNonPrimitives.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function PrimitiveNonPrimitives() {
-  return (
-    <div>PrimitiveNonPrimitives</div>
-  )
+    console.log('PrimitiveNonPrimitives');
+    const [price,setPrice]=useState(0)
+    const handleClick=()=>{
+    setPrice(price+1)
+    }
+    useEffect(()=>{console.log('use Effecttt');
+    },[price])
+    return (
+    
+        <div className='container'>
+            <button className='btn' onClick={handleClick}>Click Me!</button>
+        </div>
+      )
 }
